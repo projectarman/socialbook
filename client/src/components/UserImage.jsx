bf4f59f0074f1f9213118bbea38aa12a5c8db1cd
Soumyajit Dey <129984512+projectarman@users.noreply.github.com>
2023-04-11 12:12:34 +0530

    Update UserImage.jsx

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`http://194.195.115.145:3001/assets/${image}`}
       />
     </Box>
   );
